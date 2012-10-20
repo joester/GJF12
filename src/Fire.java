7fc62e10b8027b9d909d9b365f8495816658fed8
joester <joey_shepard@sbcglobal.net>
2012-10-20 16:28:54 -0700

    changed speed from an int to a double

--- src/Fire.java
+++ src/Fire.java
@@ -5,13 +5,12 @@ public class Fire extends Item
 	 * Damage Range Animation Speed Drop Startup, 
 	 * 3 Infinite 0.5, 4.5 bps No 0.01, 0.7-1 
 	 */
-	private int speed;
 	public Fire(int x, int y, String i)
 	{
 		super( x,  y,  i);
 		damage = 3;
 		range = 100;
-		speed = 1;
+		speed = 5;
 		dropChance = false;
 		startUpTime = .01;
 		reloadTime = .9;
